7b90503bfb8585a41337c3c878def9cd27380733
Erick11391 <xandererik4@gmail.com>
2024-12-09 22:25:20 +0300

    modified

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+
+const main = document.querySelector('main#main');
+if (main) {
+  main.remove();
+}
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'; 
+newHeader.textContent = 'Erick Mutai is the champion'; 
+
+
+document.body.appendChild(newHeader);
